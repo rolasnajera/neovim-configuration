44a8d4becae5cd17400bfd6c75ea7d62028408ee
Rolas Najera <me@rolasnajera.dev>
2025-08-13 19:34:10 +0200

    configuring lsp server

--- lua/rolasnajera/plugins/lsp/mason.lua
+++ lua/rolasnajera/plugins/lsp/mason.lua
@@ -1,15 +1,13 @@
 return {
   "williamboman/mason.nvim",
   dependencies = {
+    -- Keep mason-lspconfig installed, but do not configure it here to avoid require loops
     "williamboman/mason-lspconfig.nvim",
     "WhoIsSethDaniel/mason-tool-installer.nvim",
   },
   config = function()
     -- import mason
     local mason = require("mason")
-
-    -- import mason-lspconfig
-    local mason_lspconfig = require("mason-lspconfig")
 
     local mason_tool_installer = require("mason-tool-installer")
 
@@ -24,35 +22,7 @@ return {
       },
     })
 
-    mason_lspconfig.setup({
-      -- To see the complete list of lsp go to:  https://github.com/williamboman/mason-lspconfig.nvim/blob/main/doc/server-mapping.md
-      -- list of servers for mason to install
-      ensure_installed = {
-        "ansiblels",
-        "bashls",
-        "clangd",
-        "cssls",
-        "dockerls",
-        "emmet_ls",
-        "eslint",
-        "html",
-        "jdtls",
-        "jsonls",
-        "lua_ls",
-        "graphql",
-        "prismals",
-        "pyright",
-        "kotlin_language_server",
-        "marksman",
-        "mdx_analyzer",
-        "sqlls",
-        "tsserver",
-        "tailwindcss",
-      },
-      -- auto-install configured servers (with lspconfig)
-      automatic_installation = true, -- not the same as ensure_installed
-    })
-
+    -- Only configure external tools here; LSP servers are handled in lspconfig.lua
     mason_tool_installer.setup({
       ensure_installed = {
         "prettier", -- prettier formatter
